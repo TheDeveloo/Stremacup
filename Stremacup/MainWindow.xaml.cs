ba91650bb07d3982270821c8d6c5b8964632baf9
lanfeust129 <lanfeust129@gmail.com>
2014-04-10 17:06:18 +0200

    Added working project

---
+++ Stremacup/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Stremacup.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StreMeCup
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var em = new stremacupEntities();
+            team myteam = new team();
+            myteam.name = "Blaaaaaa";
+
+            em.team.Add(myteam);
+            em.SaveChanges();
+        }
+    }
+}
