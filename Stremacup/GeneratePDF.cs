11c856928af3c05a19176a32a018682239b01e56
lanfeust129 <lanfeust129@gmail.com>
2014-04-18 16:16:54 +0200

    PDF generated in folders, field added in matchs list

--- Stremacup/GeneratePDF.cs
+++ Stremacup/GeneratePDF.cs
@@ -24,16 +24,20 @@ namespace Stremacup
             foreach (category category in em.category)
             {
                 var pdfDoc = new Document();
-                PdfWriter.GetInstance(pdfDoc, new FileStream(category.name + ".pdf", FileMode.Create));
+                string catName = removeSpecialCharacters(category.name);
+                if(!Directory.Exists("Categories"))
+                    Directory.CreateDirectory("Categories");
+                PdfWriter.GetInstance(pdfDoc, new FileStream("Categories/"+ catName + ".pdf", FileMode.Create));
                 pdfDoc.Open();
 
                 Paragraph paragraph = new Paragraph(50f, "Matchs " + category.name);
                 paragraph.SpacingAfter = 50f;
                 pdfDoc.Add(paragraph);
 
-                PdfPTable table = new PdfPTable(4);
+                PdfPTable table = new PdfPTable(5);
                 table.WidthPercentage = 100;
                 table.AddCell("Heure");
+                table.AddCell("Terrain");
                 table.AddCell("Groupe");
                 table.AddCell("Équipe 1");
                 table.AddCell("Équipe 2");
@@ -45,6 +49,10 @@ namespace Stremacup
                         foreach (match match in team.match)
                         {
                             table.AddCell(match.datetime.ToString());
+                            if (match.field != null)
+                                table.AddCell(match.field.name);
+                            else
+                                table.AddCell("");
                             table.AddCell(match.team.group.name);
                             table.AddCell(match.team.name);
                             table.AddCell(match.team1.name);
@@ -62,7 +70,11 @@ namespace Stremacup
             foreach (group group in em.group)
             {
                 var pdfDoc = new Document();
-                PdfWriter.GetInstance(pdfDoc, new FileStream(group.name+".pdf", FileMode.Create));
+                string groupName = removeSpecialCharacters(group.name);
+                string catName = removeSpecialCharacters(group.category.name);
+                if (!Directory.Exists("Groups"))
+                    Directory.CreateDirectory("Groups");
+                PdfWriter.GetInstance(pdfDoc, new FileStream("Groups/" + groupName + "_" + catName + ".pdf", FileMode.Create));
                 pdfDoc.Open();
 
                 Paragraph paragraph = new Paragraph(50f, "Matchs "+group.name+" "+group.category.name);
@@ -72,6 +84,7 @@ namespace Stremacup
                 PdfPTable table = new PdfPTable(3);
                 table.WidthPercentage = 100;
                 table.AddCell("Heure");
+                table.AddCell("Terrain");
                 table.AddCell("Équipe 1");
                 table.AddCell("Équipe 2");
 
@@ -80,6 +93,10 @@ namespace Stremacup
                     foreach (match match in team.match)
                     {
                         table.AddCell(match.datetime.ToString());
+                        if (match.field != null)
+                            table.AddCell(match.field.name);
+                        else
+                            table.AddCell("");
                         table.AddCell(match.team.name);
                         table.AddCell(match.team1.name);
                     }
@@ -95,7 +112,10 @@ namespace Stremacup
             foreach (field field in em.field)
             {
                 var pdfDoc = new Document();
-                PdfWriter.GetInstance(pdfDoc, new FileStream(field.name + ".pdf", FileMode.Create));
+                string fieldName = removeSpecialCharacters(field.name);
+                if (!Directory.Exists("Fields"))
+                    Directory.CreateDirectory("Fields");
+                PdfWriter.GetInstance(pdfDoc, new FileStream("Fields/" + fieldName + ".pdf", FileMode.Create));
                 pdfDoc.Open();
 
                 Paragraph paragraph = new Paragraph(50f, "Matchs " + field.name);
@@ -127,28 +147,41 @@ namespace Stremacup
             foreach (team team in em.team)
             {
                 var pdfDoc = new Document();
-                PdfWriter.GetInstance(pdfDoc, new FileStream(team.name + ".pdf", FileMode.Create));
+                string name = removeSpecialCharacters(team.name);
+                string catName = removeSpecialCharacters(team.category.name);
+                if (!Directory.Exists("Teams"))
+                    Directory.CreateDirectory("Teams");
+                PdfWriter.GetInstance(pdfDoc, new FileStream("Teams/" + name + "_" + catName + ".pdf", FileMode.Create));
                 pdfDoc.Open();
 
                 Paragraph paragraph = new Paragraph(50f, "Matchs " + team.name + " " + team.category.name);
                 paragraph.SpacingAfter = 50f;
                 pdfDoc.Add(paragraph);
 
-                PdfPTable table = new PdfPTable(3);
+                PdfPTable table = new PdfPTable(4);
                 table.WidthPercentage = 100;
                 table.AddCell("Heure");
+                table.AddCell("Terrain");
                 table.AddCell("Équipe 1");
                 table.AddCell("Équipe 2");
 
                 foreach (match match in team.match)
                 {
                     table.AddCell(match.datetime.ToString());
+                    if (match.field != null)
+                        table.AddCell(match.field.name);
+                    else
+                        table.AddCell("");
                     table.AddCell(match.team.name);
                     table.AddCell(match.team1.name);
                 }
                 foreach (match match in team.match1)
                 {
                     table.AddCell(match.datetime.ToString());
+                    if (match.field != null)
+                        table.AddCell(match.field.name);
+                    else
+                        table.AddCell("");
                     table.AddCell(match.team.name);
                     table.AddCell(match.team1.name);
                 }
@@ -156,6 +189,19 @@ namespace Stremacup
                 pdfDoc.Add(table);
                 pdfDoc.Close();
             }
+        }
+
+        public string removeSpecialCharacters(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in str)
+            {
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_')
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
         }
     }
 }
